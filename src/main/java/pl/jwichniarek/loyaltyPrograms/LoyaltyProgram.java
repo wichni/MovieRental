04b0c6e881470b3b7e0584fe3d9ef46cee98a1d6
MaciejJay <50988314+MaciejJay@users.noreply.github.com>
2019-10-19 10:34:59 +0200

    Loyalty Program init commit (#1)

---
+++ src/main/java/pl/jwichniarek/loyaltyPrograms/LoyaltyProgram.java
@@ -0,0 +1,14 @@
+package pl.jwichniarek.loyaltyPrograms;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class LoyaltyProgram {
+    private Points points;
+//    private User user;
+//    private Movie movie;
+}
---
+++ src/main/java/pl/jwichniarek/loyaltyPrograms/Points.java
@@ -0,0 +1,4 @@
+package pl.jwichniarek.loyaltyPrograms;
+
+public class Points {
+}
