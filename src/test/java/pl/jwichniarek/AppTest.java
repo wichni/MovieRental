284abb7bdbb20c4a2cc0b89f9dd83ba908403fb1
Jakub Wichniarek <jakubwichniarek@yahoo.pl>
2019-10-19 09:56:10 +0200

    first commit

---
+++ src/test/java/pl/jwichniarek/AppTest.java
@@ -0,0 +1,13 @@
+package pl.jwichniarek;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+public class AppTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
